67d8631bbf9e53570ca41db7012e82abbfa5b035
J0ssue <jdiaz77711@gmail.com>
2025-04-21 10:29:29 +0100

    feature/exercise-1: implements exercise 1 solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,58 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Header = (props: { title: string }) => {
+  return <h1>{props.title}</h1>;
+};
+
+const Part = ({ name, exercises }: { name: string; exercises: number }) => {
+  return (
+    <p>
+      {name} {exercises}
+    </p>
+  );
+};
+
+const Content = ({
+  exercises,
+}: {
+  exercises: { name: string; exercises: number }[];
+}) => {
+  return (
+    <>
+      <Part name={exercises[0].name} exercises={exercises[0].exercises} />
+      <Part name={exercises[1].name} exercises={exercises[1].exercises} />
+      <Part name={exercises[2].name} exercises={exercises[2].exercises} />
+    </>
+  );
+};
+
+const Total = ({ total }: { total: number }) => {
+  return <p>Number of exercises {total}</p>;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const course = 'Half Stack application development';
+  const part1 = 'Fundamentals of React';
+  const exercises1 = 10;
+  const part2 = 'Using props to pass data';
+  const exercises2 = 7;
+  const part3 = 'State of a component';
+  const exercises3 = 14;
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Header title={course} />
+        <Content
+          exercises={[
+            { name: part1, exercises: exercises1 },
+            { name: part2, exercises: exercises2 },
+            { name: part3, exercises: exercises3 },
+          ]}
+        />
+
+        <Total total={exercises1 + exercises2 + exercises3} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
